ab6ae3e444e072c28189ed7d9b806f6fe1799912
Apurva Saraiya <apurva.saraiya@broadcom.com>
2025-06-25 13:43:08 +0530

    TNZ-46946: removes code to connect to service using sentinel

--- lib/client.rb
+++ lib/client.rb
@@ -15,51 +15,6 @@ class ClientRedis
     )
   end
 
-  def self.using_sentinel(credentials)
-      Redis.new(
-        name: credentials.fetch('master_name'),
-        password: credentials.fetch('password'),
-        sentinel_password: credentials.fetch('sentinel_password'),
-        sentinels: credentials.fetch('sentinels').map { | sentinel |  { host: sentinel["host"], port: sentinel["port"] } },
-        timeout: 30
-      )
-    end
-
-  def self.tls_using_sentinel(credentials, version='')
-    if version.include?("TLSv1_3")
-      master_redis = SentinelTLS13.new(
-        host: credentials.fetch('sentinels').first['host'],
-        port: credentials.fetch('sentinels').first['tls_port'],
-        sentinel_password: credentials.fetch("sentinel_password"),
-        master_name: credentials.fetch('master_name')
-      ).get_redis_instance
-      return @client_tls_13 ||= RedisTLS13.new(
-        host: master_redis[0],
-        port: master_redis[1],
-        password: credentials.fetch('password')
-      )
-    else
-      ssl_params = {
-        verify_mode: OpenSSL::SSL::VERIFY_NONE,
-        ssl_version: version,
-      }
-      if version.empty?
-        ssl_params = {
-          verify_mode: OpenSSL::SSL::VERIFY_NONE
-        }
-      end
-      Redis.new(
-        name: credentials.fetch('master_name'),
-        password: credentials.fetch('password'),
-        sentinel_password: credentials.fetch('sentinel_password'),
-        sentinels: credentials.fetch('sentinels').map { | sentinel |  { host: sentinel["host"], port: sentinel["tls_port"] } },
-        ssl: true,
-        ssl_params: ssl_params,
-        timeout: 30
-      )
-    end
-  end
-
   def self.tls(credentials, version='')
     ssl_params = {
       verify_mode: OpenSSL::SSL::VERIFY_NONE,
